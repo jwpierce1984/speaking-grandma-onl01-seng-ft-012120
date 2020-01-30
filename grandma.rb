5ee2c592e6d275c621bb4aa78515e20af8605eca
Justin Pierce <jwpierce1984@gmail.com>
2020-01-30 23:20:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 def speak_to_grandma(phrase)
   if
-    phrase == phrase.upcase
+    phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   elsif
-    phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN"
+    phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     phrase!=phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
